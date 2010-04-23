932c68a3496a70ee9797da82f63a376c1377fbef
Justin Hileman <justin@justinhileman.info>
2010-04-23 11:48:02 -0400

    Merging changes from HandlebarMustache

--- Mustache.php
+++ Mustache.php
@@ -24,12 +24,32 @@ class Mustache {
 
 	// Override charset passed to htmlentities() and htmlspecialchars(). Defaults to UTF-8.
 	protected $charset = 'UTF-8';
-	
+
 	protected $tagRegEx;
 
 	protected $template = '';
 	protected $context  = array();
 	protected $partials = array();
+
+	/**
+	 * templateBase directory.
+	 *
+	 * If none is specified, this will default to `dirname(__FILE__)`.
+	 *
+	 * @var string
+	 * @access protected
+	 */
+	protected $templateBase;
+
+	/**
+	 * templateName.
+	 *
+	 * If none is specified, this will default to an underscorified version of the class name.
+	 *
+	 * @var string
+	 * @access protected
+	 */
+	protected $templateName;
 
 	/**
 	 * Mustache class constructor.
@@ -47,7 +67,65 @@ class Mustache {
 		if ($template !== null) $this->template = $template;
 		if ($partials !== null) $this->partials = $partials;
 		if ($view !== null)     $this->context = array($view);
+
+		// default template base is the current directory.
+		if (!isset($this->templateBase)) {
+			$this->setTemplateBase(dirname(__FILE__));
+		}
+
+		// default template name is the underscorified class name.
+		if (!isset($this->templateName)) {
+			$this->templateName = strtolower(preg_replace('#(?<!^)([A-Z]+)#', '_\1', get_class($this)));
+		}
+
 	}
+
+	/**
+	 * Override the current templateBase.
+	 *
+	 * @access public
+	 * @param string $dir
+	 * @return void
+	 */
+	public function setTemplateBase($dir) {
+		if (substr($dir, -1) !== '/') {
+			$dir .= '/';
+		}
+		$this->templateBase = $dir;
+	}
+
+	/**
+	 * Override the default templateName.
+	 *
+	 * @access public
+	 * @param string $name
+	 * @return void
+	 */
+	public function setTemplateName($name) {
+		$this->templateName = $name;
+	}
+
+	/**
+	 * Load a template file. This file will be relative to $this->templateBase.
+	 * A '.mustache' file extension is assumed if none is provided in $file.
+	 *
+	 * @access public
+	 * @param string $name
+	 * @return void
+	 */
+	public function loadTemplate($name) {
+		if (strpos($name, '.') === false) {
+			$name .= '.mustache';
+		}
+
+		$filename = $this->templateBase . $name;
+		if (file_exists($filename)) {
+			$this->template = file_get_contents($filename);
+		} else {
+			$this->template = null;
+		}
+	}
+
 
 	/**
 	 * Render the given template and view object.
@@ -62,7 +140,15 @@ class Mustache {
 	 * @return string Rendered Mustache template.
 	 */
 	public function render($template = null, $view = null, $partials = null) {
-		if ($template === null) $template = $this->template;
+
+		// Autoload template if none is explicitly set.
+		if ($template === null) {
+			if (!isset($this->template)) {
+				$this->loadTemplate($this->templateName);
+			}
+			$template = $this->template;
+		}
+
 		if ($partials !== null) $this->partials = $partials;
 
 		if ($view) {
@@ -355,7 +441,9 @@ class Mustache {
 	/**
 	 * Retrieve the partial corresponding to the requested tag name.
 	 *
-	 * Silently fails (i.e. returns '') when the requested partial is not found.
+	 * Load a partial, either from $this->partials or from a file in the templateBase
+	 * directory. If throwPartialExceptions is not enabled, getPartial() silently fails
+	 * (i.e. returns '') when the requested partial is not found.
 	 *
 	 * @access protected
 	 * @param string $tag_name
@@ -367,11 +455,18 @@ class Mustache {
 			return $this->partials[$tag_name];
 		}
 
+		// If no partial is explictly set, search for an appropriately named partial template file.
+		$filename = $this->templateBase . $tag_name . '.mustache';
+		if (file_exists($filename)) {
+			$this->partials[$tag_name] = file_get_contents($filename);
+			return $this->partials[$tag_name];
+		}
+
 		if ($this->throwPartialExceptions) {
 			throw new MustacheException('Unknown partial: ' . $tag_name, MustacheException::UNKNOWN_PARTIAL);
-		} else {
-			return '';
 		}
+
+		return '';
 	}
 
 	/**
